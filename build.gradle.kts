8299977a82f1443b81646a8dc84abecf0306c702
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-05 12:56:13 +0530

    copy multi file

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,3 +22,14 @@ tasks.register<Copy>("copyReport") {
     from(layout.projectDirectory.file("reports/my-report.pdf"))
     into(layout.projectDirectory.dir("toArchive"))
 }
+
+tasks.register<Copy>("copyReport2") {
+    from(layout.buildDirectory.file("reports/my-report.pdf"))
+    into(layout.buildDirectory.dir("toArchive2"))
+}
+
+//copy multi file
+tasks.register<Copy>("copyReportsForArchiving") {
+    from(layout.projectDirectory.file("reports/my-report.pdf"), layout.projectDirectory.file("src/docs/manual.pdf"))
+    into(layout.buildDirectory.dir("reports"))
+}
