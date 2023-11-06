8e8cb5591da67c5ac096e38d31e72a7d710767fd
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-06 13:41:45 +0530

    Specifying a set of files

--- build.gradle.kts
+++ build.gradle.kts
@@ -321,3 +321,24 @@ val tar: FileTree = tarTree("thirdPartyResources.tar")
 //    println(file.name)
 //}
 
+//Specifying a set of files
+tasks.register<JavaCompile>("compile") {
+    // Use a File object to specify the source directory
+    source = fileTree(file("src/main/java"))
+
+    // Use a String path to specify the source directory
+    source = fileTree("src/main/java")
+
+    // Use a collection to specify multiple source directories
+    source = fileTree(listOf("src/main/java", "../shared/java"))
+
+    // Use a FileCollection (or FileTree in this case) to specify the source files
+    source = fileTree("src/main/java").matching { include("org/gradle/api/**") }
+
+    // Using a closure to specify the source files.
+    setSource({
+        // Use the contents of each zip file in the src dir
+        file("src").listFiles()!!.filter { it.name.endsWith(".zip") }.map { zipTree(it) }
+    })
+}
+
