44f6848b1c7f5c51de0de602533f0f08f1093f34
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-06 12:34:27 +0530

    Using a file tree

--- build.gradle.kts
+++ build.gradle.kts
@@ -282,13 +282,31 @@ var tree: ConfigurableFileTree = fileTree("src/main")
 // Add include and exclude patterns to the tree
 tree.include("**/*.java")
 tree.exclude("**/Abstract*")
-// Create a tree using closure
-tree = fileTree("src") {
-    include("**/*.java")
-}
+//// Create a tree using closure
+//tree = fileTree("src") {
+//    include("**/*.java")
+//}
 // Create a tree using a map
-tree = fileTree("dir" to "src", "include" to "**/*.java")
-tree = fileTree("dir" to "src", "includes" to listOf("**/*.java", "**/*.xml"))
-tree = fileTree("dir" to "src", "include" to "**/*.java", "exclude" to "**/*test*/**")
+//tree = fileTree("dir" to "src", "include" to "**/*.java")
+//tree = fileTree("dir" to "src", "includes" to listOf("**/*.java", "**/*.xml"))
+//tree = fileTree("dir" to "src", "include" to "**/*.java", "exclude" to "**/*test*/**")
 
 // Using a file tree
+// Iterate over the contents of a tree
+//tree.forEach{ file: File ->
+//    println(file)
+//}
+
+// Filter a tree
+val filtered: FileTree = tree.matching {
+    include("org/gradleTest/**")
+}
+
+// Add trees together
+val sum: FileTree = tree + fileTree("src/test")
+
+// Visit the elements of the tree
+/*
+tree.visit {
+    println("${this.relativePath} => ${this.file}")
+}*/
