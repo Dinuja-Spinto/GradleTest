90f12044298207fb71ca878f55d096e03b2aade9
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-07 14:24:17 +0530

    Connecting properties together

--- build.gradle.kts
+++ build.gradle.kts
@@ -831,3 +831,26 @@ tasks.register<Greeting>("greetingHi") {
     greeting.set("Hi1")
     greeting = "Hi2"
 }
+
+//Connecting properties together
+// A project extension
+interface MessageExtension {
+    // A configurable greeting
+    abstract val greeting: Property<String>
+}
+
+// Create the project extension
+val messages = project.extensions.create<MessageExtension>("messages")
+
+// Create the greeting task
+tasks.register<Greeting>("greetinghi2") {
+    // Attach the greeting from the project extension
+    // Note that the values of the project extension have not been configured yet
+    greeting = messages.greeting
+}
+
+messages.apply {
+    // Configure the greeting on the extension
+    // Note that there is no need to reconfigure the task's `greeting` property. This is automatically updated as the extension property changes
+    greeting = "Hello"
+}
