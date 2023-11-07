4dccf807e10ee881df6c33a6755124365c3b8299
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-07 14:43:28 +0530

    Map property

--- build.gradle.kts
+++ build.gradle.kts
@@ -935,3 +935,30 @@ producerTwo { outputFile = layout.buildDirectory.file("two.txt") }
 // Don't need to update the task properties. These are automatically updated as the build directory changes
 layout.buildDirectory = layout.projectDirectory.dir("output")
 
+//Map property
+abstract class Generator: DefaultTask() {
+    @get:Input
+    abstract val properties: MapProperty<String, Int>
+
+    @TaskAction
+    fun generate() {
+        properties.get().forEach { entry ->
+            logger.quiet("${entry.key} = ${entry.value}")
+        }
+    }
+}
+
+// Some values to be configured later
+var b = 0
+var c = 0
+
+tasks.register<Generator>("generate2") {
+    properties.put("a", 1)
+    // Values have not been configured yet
+    properties.put("b", providers.provider { b })
+    properties.putAll(providers.provider { mapOf("c" to c, "d" to c + 1) })
+}
+
+// Configure the values. There is no need to reconfigure the task
+b = 2
+c = 3
