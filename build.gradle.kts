323a8377d3139996d0a40fe9f1cf44d9a242b52d
Dinuja Pinto <sri.prabasha@gmail.com>
2023-11-07 13:17:33 +0530

    incremental task

--- build.gradle.kts
+++ build.gradle.kts
@@ -754,3 +754,61 @@ tasks.register<GreetingTask2>("hello5")
 tasks.register<GreetingTask2>("greeting") {
     greeting = "greetings from GreetingTask"
 }
+
+//Defining an incremental task action
+abstract class IncrementalReverseTask : DefaultTask() {
+    @get:Incremental
+    @get:PathSensitive(PathSensitivity.NAME_ONLY)
+    @get:InputDirectory
+    abstract val inputDir: DirectoryProperty
+
+    @get:OutputDirectory
+    abstract val outputDir: DirectoryProperty
+
+    @get:Input
+    abstract val inputProperty: Property<String>
+
+    @TaskAction
+    fun execute(inputChanges: InputChanges) {
+        println(
+                if (inputChanges.isIncremental) "Executing incrementally"
+                else "Executing non-incrementally"
+        )
+
+        inputChanges.getFileChanges(inputDir).forEach { change ->
+            if (change.fileType == FileType.DIRECTORY) return@forEach
+
+            println("${change.changeType}: ${change.normalizedPath}")
+            val targetFile = outputDir.file(change.normalizedPath).get().asFile
+            if (change.changeType == ChangeType.REMOVED) {
+                targetFile.delete()
+            } else {
+                targetFile.writeText(change.file.readText().reversed())
+            }
+        }
+    }
+}
+
+// Running the incremental task for the first time
+tasks.register<IncrementalReverseTask>("incrementalReverse") {
+    inputDir = file("src/main/resources")
+    outputDir = layout.buildDirectory.dir("outputs")
+    inputProperty = project.findProperty("taskInputProperty") as String? ?: "original"
+}
+//Running the incremental task with updated input files
+tasks.register("updateInputs") {
+    val inputsDir = layout.projectDirectory.dir("src/main/resources")
+    outputs.dir(inputsDir)
+    doLast {
+        inputsDir.file("1.txt").asFile.writeText("Changed content for existing file 1.")
+        inputsDir.file("2.txt").asFile.writeText("Content for new file 2.")
+    }
+}
+//Running the incremental task with an input file removed
+tasks.register<Delete>("removeInput") {
+    delete("src/main/resources/1.txt")
+}
+//Running the incremental task with an output file removed
+tasks.register<Delete>("removeOutput") {
+    delete(layout.buildDirectory.file("outputs/2.txt"))
+}
